1fe9db9f44c4250961c07612ae48cd40b9a903d0
jihyuk0414 <dealon2580@naver.com>
2024-07-13 13:32:47 +0900

    querydsl 제거 + 의존성 설정

--- src/main/java/org/example/ApiGatewayApplication.java
+++ src/main/java/org/example/ApiGatewayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@EnableDiscoveryClient
