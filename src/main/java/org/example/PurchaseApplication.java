319f78ebd296aba539978f74c270437824d9a2b3
stophyeon <jeongjihyeon401@gmail.com>
2024-07-18 14:37:24 +0900

    Swagger 의존성 제거

--- src/main/java/org/example/PurchaseApplication.java
+++ src/main/java/org/example/PurchaseApplication.java
@@ -3,15 +3,9 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableFeignClients
-@EnableDiscoveryClient
-@RefreshScope
 @EnableAspectJAutoProxy
 public class PurchaseApplication {
     public static void main(String[] args) {
