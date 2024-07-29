2725a0a311640caaeff911200e0c9ec72e351369
stophyeon <jeongjihyeon401@gmail.com>
2024-07-29 21:05:19 +0900

    Create member branch

--- src/main/java/org/example/dto/member/MemberDto.java
+++ src/main/java/org/example/dto/member/MemberDto.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class MemberDto {
-
     @Email
     private String email;
 
