a9680a80f87f061ccf6762723141d8c5a24c8216
stophyeon <jeongjihyeon401@gmail.com>
2024-08-05 14:00:22 +0900

    url 수정

---
+++ src/main/java/org/example/repository/ChatRepository.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.Chatting;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ChatRepository extends MongoRepository<Chatting, String> {
+    List<Chatting> findByChatRoomId(String room);
+
+}
