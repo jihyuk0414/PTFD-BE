13a61325dc3d347ebdc0b7b53a0dd1e1ae076b4b
jihyuk0414 <dealon2580@naver.com>
2024-08-25 15:58:37 +0900

    update : ci/cd 증빙 자료 구현을 위한 PUSH

--- src/main/java/org/example/PurchaseApplication.java
+++ src/main/java/org/example/PurchaseApplication.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -7,8 +8,10 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableAspectJAutoProxy
+@Slf4j
 public class PurchaseApplication {
     public static void main(String[] args) {
+        log.info("contest16: this is for proving ci/cd ");
         SpringApplication.run(PurchaseApplication.class, args);
     }
 }
