51094e400dde69e30e282ec2ea0a41759d177e79
jihyuk0414 <dealon2580@naver.com>
2024-09-10 22:32:29 +0900

    update : chatroom으로 변경

--- src/main/java/org/example/service/chatting/ChatFeign.java
+++ src/main/java/org/example/service/chatting/ChatFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "chatApi",url = "http://chat-service:50/chat")
+@FeignClient(name = "chatApi",url = "http://chat-service:50/chatroom")
 public interface ChatFeign {
 
     @PostMapping("/nick_name")
