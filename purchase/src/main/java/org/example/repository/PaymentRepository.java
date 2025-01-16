7e4219bbb334e8fef49dd419fcdef373aead906b
jihyuk0414 <dealon2580@naver.com>
2025-01-16 21:34:05 +0900

    refactor : 추적 파일 수정

--- purchase/src/main/java/org/example/repository/PaymentRepository.java
+++ purchase/src/main/java/org/example/repository/PaymentRepository.java
@@ -2,7 +2,7 @@ package org.example.repository;
 
 
 import org.example.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
